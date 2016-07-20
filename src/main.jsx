2e710f566afca2fb28a7f19fa05dbd61e6ea3ff2
Sasankh BC <bcsasankh@gmail.com>
2016-07-20 00:04:22 -0700

    initial code

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var NavBar = require('./components/nav/NavBar.jsx');
+
+var navLinks = [
+  {
+    title: 'Home',
+    href: '#'
+  },
+  {
+    title: 'Course',
+    href: '#'
+  },
+  {
+    title: 'Blog',
+    href: '#'
+  }
+];
+
+ReactDOM.render(<NavBar bgColor='#FFF' titleColor='#3097d1' navData={navLinks}/>, document.getElementById('nav'));
